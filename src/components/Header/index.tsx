928e1d72c24f48e5ef99f8fad49240f5a64a6fd7
unknown <alemaobrk1@gmail.com>
2022-05-09 18:18:39 -0300

    cria header e layout

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export const Header = () => (
+  <header>
+    <nav>
+      <Link to='/'>AutoLuby</Link>
+      <Link to='/'>Sair</Link>
+    </nav>
+  </header>
+)
