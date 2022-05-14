e93f38109693709c91ab8561095f7f9ba2ebc1f3
unknown <alemaobrk1@gmail.com>
2022-05-14 10:42:37 -0300

    #15 cria contexto de vídeos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "../components/Layout";
+import { VehiclesProvider } from "../context/Vehicles";
 import { CarsPage } from "../pages/CarsPage";
 import { EmployeeListPage } from "../pages/EmployeeListPage";
 import { HomePage } from "../pages/HomePage";
@@ -12,12 +13,22 @@ export const Router = () => (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/carros" element={<CarsPage />} />
-        <Route path="/carros-reservados" element={<ReservedCarsPage />} />
+
+        <Route path="/carros" element={
+          <VehiclesProvider>
+            <CarsPage />
+          </VehiclesProvider>
+        } />
+        <Route path="/carros-reservados" element={
+          <VehiclesProvider>
+            <ReservedCarsPage />
+          </VehiclesProvider>
+        } />
         <Route path="/lista-de-funcionarios" element={<EmployeeListPage />} />
       </Route>
 
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={ <LoginPage /> } />
+
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>
