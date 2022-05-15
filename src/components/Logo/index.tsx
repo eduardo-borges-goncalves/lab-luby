c0dde0cd997494328d56aeb19bfe838d14f3a1c1
unknown <alemaobrk1@gmail.com>
2022-05-15 20:05:13 -0300

    #18 implementa tela de login

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,8 +2,12 @@ import { ReactComponent as AutoLuby } from "../../assets/logo/AutoLuby.svg"
 import { LogoIcon } from "../LogoIcon"
 import { LogoWrapper } from "./styles"
 
-export const Logo = () => (
-  <LogoWrapper>
+type LogoProps ={
+  id?:string, 
+}
+
+export const Logo = ({id}:LogoProps) => (
+  <LogoWrapper id={id}>
     <LogoIcon/> 
     <AutoLuby className="autoLuby"/>
   </LogoWrapper>
